24cdaf3d764513945f3fd771d9ee0ac01e6e9fb1
TheBenMcGuire <96052774+TheBenMcGuire@users.noreply.github.com>
2022-02-08 19:40:40 +0000

    Add files via upload

---
+++ computing-work-main/drinksmachine.py
@@ -0,0 +1,44 @@
+import random
+import time         #fun setup code
+selecting = True
+
+#list of drinks
+drinks = 'COCA-COLA,DIET COKE,COKE ZERO,DR PEPPER,DR PEPPER ZERO,IRN BRU,IRN BRU XTRA,MONSTER ORIGINAL,MONSTER NITRO,MONSTER MANGO LOCO,MONSTER MONARCH,MONSTER ULTRA,CAPRI SUN,BLOOD OF INNOCENTS,DIET BLOOD OF INNOCENTS,CHERRY BLOOD OF INNOCENTS,BLOOD OF INNOCENTS LIME TWIST,JUST WATER,STRAWBERRY MILKSHAKE,MYSTERY SPECIAL'.split(',')
+
+dispenseASCII = '''===========
+DISPENSING:
+     |
+     |
+     |
+    ===
+{0}
+    ==='''
+
+while selecting:
+    
+    try:
+        choicedigit1 = input('Please select your desired drink as a two digit number (digit 1) ')
+        choicedigit2 = input('Please select your desired drink (digit 2) ')
+    
+        choice = int(choicedigit1 + choicedigit2) - 1       #choosing the drink
+
+        confirming = True
+
+        if choice > 19 or choice < 0:
+            print('The drinks machine only goes from 01 to 20!')
+            confirming = False
+
+        while confirming:       #confirmation of drink
+            confirmation = input('Do you want a '+drinks[choice]+' ?  OK or CANCEL ').upper()
+
+            if confirmation == 'OK':
+                confirming = False
+                selecting = False
+
+                print(dispenseASCII.format(drinks[choice]))
+
+            if confirmation == 'CANCEL':
+                confirming = False
+
+    except ValueError:
+        print('Please only enter numbers!')
